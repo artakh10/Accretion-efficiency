741b8a67d873acc656ad3a13034644e41f33536c
Arta <100132784+artakh10@users.noreply.github.com>
2024-02-22 13:48:04 +0330

    Replace constants to match your desired data

---
+++ constants_Acc.py
@@ -0,0 +1,16 @@
+""""
+Physical and cosmological constants
+Author: Arta Khosravi
+Started: 
+Last update: Jan. 2024
+"""
+import numpy as np
+
+c = 3e+5 #Speend of light #kms-1
+H0 = (69.7) #Hubble constant
+epsilon_const=0.1 #Standard Accretion disk efficiency
+kbol=22.4 #For L_X
+cosi=0.8
+Msun=2e+30 #kg
+yr=3.156e+7 #s
+G=6.67*1e-20 #km3 kg-1 s-2
